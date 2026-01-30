2111b2c6efe7e18525aaa3437a08ed11119ce94f
Baldives <bnaseem@yahoo.com>
2026-01-30 13:22:37 +0500

    Update main.js

--- apify/main.js
+++ apify/main.js
@@ -1,52 +1,33 @@
 import { Actor } from 'apify';
-import { execFile } from 'child_process';
-import path from 'path';
-import util from 'util';
-
-const execFileAsync = util.promisify(execFile);
+import { execSync } from 'child_process';
 
 await Actor.init();
 
 const input = await Actor.getInput();
 
 const {
-    type,
-    query,
-    number,
-    filetype,
-    download,
-    sessionFile,
+  type,
+  query,
+  number,
+  download,
+  filetype,
+  sessionFile,
+  proxy
 } = input;
 
-// Validate minimal input
-if (!type || !query) {
-    throw new Error('Both "type" and "query" are required');
+let command = `node cli.js ${type}`;
+
+if (query) command += ` ${query}`;
+if (number) command += ` -n ${number}`;
+if (download) command += ` -d`;
+if (filetype) command += ` -t ${filetype}`;
+if (sessionFile) command += ` --session-file ${sessionFile}`;
+if (proxy === 'RESIDENTIAL') {
+  command += ` --proxy ${process.env.APIFY_PROXY_URL}`;
 }
 
-// Build CLI arguments
-const args = [
-    type,                 // hashtag
-    query,                // funnycats
-    '--number', String(number || 10),
-    '--filetype', filetype || 'json',
-    '--session-file', sessionFile || '',
-];
+console.log('Running:', command);
 
-// Explicitly prevent downloads
-if (download === false) {
-    // do nothing, default is no download
-}
-
-// Run scraper CLI
-const cliPath = path.join(process.cwd(), 'cli.js');
-
-const { stdout, stderr } = await execFileAsync(
-    'node',
-    [cliPath, ...args],
-    { maxBuffer: 1024 * 1024 * 10 }
-);
-
-if (stdout) console.log(stdout);
-if (stderr) console.error(stderr);
+execSync(command, { stdio: 'inherit' });
 
 await Actor.exit();
